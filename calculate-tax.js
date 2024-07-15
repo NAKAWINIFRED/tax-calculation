fcdc413c0920dddcc7371841134b6ec7f44d170b
Isaac Kalumba <ikalumba221@gmail.com>
2024-07-16 00:00:51 +0300

    refactor

--- calculate-tax.js
+++ calculate-tax.js
@@ -1,12 +1,20 @@
-export function calculateTax(earnings){
+export function calculateTax(earnings) {
+  try {
     if (earnings <= 12000) {
-        return 0;
+      return 0;
+    } else if (earnings <= 36000) {
+      return earnings * 0.2;
+    } else if (earnings > 36000) {
+      return earnings * 0.4;
+    } else if (earnings <= 0) {
+      throw new Error("Earnings have to be greater than zero");
+    } else if (isNaN(earnings)) {
+      throw new Error("The value entered is not a number");
+    } else {
+      throw new Error("Error calculating tax");
     }
-    else if (earnings <= 36000) {
-        return earnings * 0.2;
-    }
-    else if (earnings > 36000) {
-        return earnings * 0.4;
-    }
-
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
--- test/test.js
+++ test/test.js
@@ -2,14 +2,15 @@ import { assert } from "chai";
 import { calculateTax } from "../calculate-tax.js";
 import { describe, it } from "mocha";
 
-describe('taxcalculation' , function() {
-    it('should return no pay for earns less than 12,000' , function(){
-      assert.equal(calculateTax(12000), 0)
-    });
-    it('should return 20% tax for earns between 12,000 and  36,000' , function(){
-        assert.equal(calculateTax(30000), 0.2*30000)
-      });
-    it('should return 40% tax for earns higher than 36,000' , function(){
-        assert.equal(calculateTax(40000), 0.4*40000)
-      });
+describe("taxcalculation", function () {
+  it("should return no pay for earns less than 12,000", function () {
+    assert.equal(calculateTax(12000), 0);
+  });
+  it("should return 20% tax for earns between 12,000 and  36,000", function () {
+    assert.equal(calculateTax(30000), 0.2 * 30000);
+  });
+  it("should return 40% tax for earns higher than 36,000", function () {
+    assert.equal(calculateTax(40000), 0.4 * 40000);
+  });
+  
 });
