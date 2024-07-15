e3a74e88ce4b14b35e3d85a2617dd96951868e22
NAKAWINIFRED <winifrednakawunde07@gmail.com>
2024-07-15 17:29:43 +0300

    run second passing test

--- calculate-tax.js
+++ calculate-tax.js
@@ -2,5 +2,8 @@ export function calculateTax(earnings){
     if (earnings <= 12000) {
         return 0;
     }
+    else if (earnings <= 36000) {
+        return (earnings  ) * 0.2;
+    }
 
 }
--- test/test.js
+++ test/test.js
@@ -6,5 +6,7 @@ describe('taxcalculation' , function() {
     it('should return no pay for earns less than 12000' , function(){
       assert.equal(calculateTax(12000), 0)
     });
-    
+    it('should return 20% tax for earns between 12000 and  36000' , function(){
+        assert.equal(calculateTax(30000), 0.2*30000)
+      });
 });
